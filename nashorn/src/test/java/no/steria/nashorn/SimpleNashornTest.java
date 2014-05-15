c5ffa4157b8160dfde69d47237ea388b152d69b3
Kim A. Martinsen <ka.martinsen@gmail.com>
2014-05-15 13:13:33 +0200

    Added an extra test for Nashorn

--- nashorn/src/test/java/no/steria/nashorn/SimpleNashornTest.java
+++ nashorn/src/test/java/no/steria/nashorn/SimpleNashornTest.java
@@ -16,6 +16,14 @@ public class SimpleNashornTest {
     }
 
     @Test
+    public void should_reply_when_Java8_calls() throws Exception {
+        SimpleNashorn simpleNashorn = new SimpleNashorn();
+        HelloNashorn helloNashorn = simpleNashorn.loadHelloNashorn();
+
+        assertThat(helloNashorn.sayHello("Java8")).isEqualTo("Hello Java8");
+    }
+
+    @Test
     public void should_turn_on_lightbulb_using_nashorn() throws Exception {
         SimpleNashorn simpleNashorn = new SimpleNashorn();
         HelloNashorn helloNashorn = simpleNashorn.loadHelloNashorn();
